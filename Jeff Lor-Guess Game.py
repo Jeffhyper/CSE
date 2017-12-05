767d3708cdc194ebe62bf575660a8f7801206fde
Jeffhyper <31043546+Jeffhyper@users.noreply.github.com>
2017-12-05 09:45:17 -0800

    Added input

--- Jeff Lor-Guess Game.py
+++ Jeff Lor-Guess Game.py
@@ -1,9 +1,15 @@
 # Remember, INPUTS FROM USERS ARE ALWAYS (!!!)
 # OF TYPE STRING!!!!
 
+# 1. Generate a Number
+# 2. Ask the user for an input(Number)
+# 3. Does the Guess match the number?
+# 4. Add "Higher" and "Lower"
+# 5. Add 5 guesses
+
 # random Numbers
 import random
 print(random.randint(1,50))
 
-def
+input(24)
 
--- Warmup.py
+++ Warmup.py
@@ -11,3 +11,14 @@ def reverse_order(firstname, lastname):
     # print("%s, %s"  % (lastname, firstname))
     print(last_name + " " + first_name) # Concatenation
 
+
+
+# 12.5.17
+"""Write a function called add_py
+that takes one parameter called "name"
+and prints out name_p
+
+
+def add_py(Mike):
+    print("%s.py" % name) # Solution 1
+    print(name + ".py")  #Solution 2
